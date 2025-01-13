50e07da81709adfa51f5fdc9d55a7a9d3ca3d8f9
ksk <133188669+ksk-117@users.noreply.github.com>
2025-01-14 02:17:33 +0900

    投稿詳細ページの作成、DOMPurifyによるHTMLサニタイズの実装、PostSummaryコンポーネントのリンク追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "w1980.blob.core.windows.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/_components/PostSummary.tsx
+++ src/app/_components/PostSummary.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import type { Post } from "@/app/_types/Post";
 
 type Props = {
@@ -8,9 +9,27 @@ type Props = {
 const PostSummary: React.FC<Props> = (props) => {
   const { post } = props;
   return (
-    <div className="border border-slate-400 p-3">
-      <div className="font-bold">{post.title}</div>
-      <div>{post.content}</div>
+    <div className="relative border border-slate-400 p-3">
+      <div className="text-sm text-gray-500">
+        {new Date(post.createdAt).toLocaleDateString()}
+      </div>
+      <Link href={`/posts/${post.id}`}>
+        <div className="mb-1 text-lg font-bold">{post.title}</div>
+        <div
+          className="line-clamp-3"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </Link>
+      <div className="absolute right-3 top-3 flex space-x-2">
+        {post.categories.map((category) => (
+          <span
+            key={category.id}
+            className="rounded border border-gray-200 px-1 py-0.5 text-xs text-gray-500"
+          >
+            {category.name}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
